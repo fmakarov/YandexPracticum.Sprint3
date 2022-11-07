1bb439699ef954fc05a41e0b34147fdc4f13befd
fedro <fjodor.makaroff@gmail.com>
2022-11-07 04:58:58 +0300

    Initial Commit

---
+++ YandexPracticum.Sprint3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YandexPracticum.Sprint3
+//
+//  Created by paul kellerman on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
