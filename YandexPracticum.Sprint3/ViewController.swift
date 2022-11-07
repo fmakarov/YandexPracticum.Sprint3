b97d8c3ddeffd299d077befb3337b1d8f75bcd66
fedro <fjodor.makaroff@gmail.com>
2022-11-07 06:00:41 +0300

    first init

--- YandexPracticum.Sprint3/ViewController.swift
+++ YandexPracticum.Sprint3/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonDown: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        buttonDown.backgroundColor = .darkGray
+        buttonDown.tintColor = .white
     }
+    
+    var counter = 0
 
-
+    @IBAction func buttonClick(_ sender: Any) {
+        counter += 1
+        counterLabel.text = String(counter)
+        print(counter)
+    }
+    
+    private var isClick: Bool = false
+    
 }
 
